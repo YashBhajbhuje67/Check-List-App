9138edbef25d5a8ae6e7943ca27ccd9192299165
Yash Bhajbhuje <yashbhajbhuje67@gmail.com>
2023-03-04 19:07:45 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://check-list-app-coral.vercel.app/';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
